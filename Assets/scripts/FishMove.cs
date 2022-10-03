aa37b9bd970b93936163081b02617f17ea1915f8
ha77y <114589921+ha77y@users.noreply.github.com>
2022-10-03 16:32:42 +0100

    a

--- Assets/scripts/FishMove.cs
+++ Assets/scripts/FishMove.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FishMove : MonoBehaviour
 {
+    private  float leftborder = 4;
+    private float rightborder = 6;
+    private bool left;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,26 @@ public class FishMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (gameObject.transform.position.x < leftborder)
+        {
+            left = false;
+        }
+        else if (gameObject.transform.position.x >= rightborder)
+        {
+            left = true;
+        }
+
+        if(left == false )
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 0.005f, gameObject.transform.position.y);
+        }
+        else
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x - 0.005f, gameObject.transform.position.y);
+        }
+
+
+
     }
 }

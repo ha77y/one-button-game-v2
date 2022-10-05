c4b2cd9501e158c43e26c775aeefb5f845d367e0
ha77y <114589921+ha77y@users.noreply.github.com>
2022-10-05 18:44:07 +0100

    progress update 1
    code non functional as is, issue with colliders

--- Assets/scripts/hook.cs
+++ Assets/scripts/hook.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class hook : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static float completion;
+    public static bool On = false;
+    public static int wins;
+    [SerializeField] private Transform Progress;
 
-    // Update is called once per frame
+
     void Update()
     {
+        Progress.position = new Vector3(Progress.position.x, (Progress.position.y - (3f - completion)));
+
+        if (On == true)
+        {
+            if (completion > 3f)
+            {
+                On = false;
+                wins = wins + 1;
+                completion = 0f;
+            }
+            else if (completion > 0f)
+            { 
+                completion = completion - 0.1f;
+            }
+            else
+            {
+                On = false;
+                completion = 0f;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+
+            {
+                completion += 0.5f;
+                
+            } 
+        }
+    }
+
+    private void FixedUpdate()
+    {
         
     }
+   /* private void OnTriggerEnter(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            On = true;
+            completion = 0.5f;
+        }
+        
+    } */
 }
